34b422c9919cadc54c63025718f4bd7bfb5031ff
TwitchSolvesCube <TwitchSolvesCube@tuta.io>
2022-01-14 12:18:15 -0800

    Removed arrays and channel name

--- TSC.ts
+++ TSC.ts
@@ -1,33 +1,4 @@
-import { randomScrambleForEvent } from "cubing/scramble"
-//Global constants
-export const channelName = "TwitchSolvesBot";
-
-// Array of all supported moves
-export const moves333 =
-    ["R", "R'", "R2", "r", "r'", "r2",
-        "L", "L'", "L2", "l", "l'", "l2",
-        "F", "F'", "F2", "f", "f'", "f2",
-        "B", "B'", "B2", "b", "b'", "b2",
-        "D", "D'", "D2", "d", "d'", "d2",
-        "U", "U'", "U2", "u", "u'", "u2",
-        "E", "E'", "E2",
-        "S", "S'", "S2",
-        "M", "M'", "M2",
-        "x", "x'", "x2",
-        "y", "y'", "y2",
-        "z", "z'", "z2"];
-
-export const snMoves333 =
-    ["i", "k", "u", "m",
-        "d", "e", "v", "r",
-        "h", "g",
-        "w", "o",
-        "s", "l", "z", "?",
-        "j", "f", ",", "c",
-        "5", "6", "x",
-        "t", "y", "b",
-        ";", "a",
-        "p", "q"]
+import { randomScrambleForEvent } from "cubing/scramble";
 
 export default class TSC {
     public scramble: Array<string>;
