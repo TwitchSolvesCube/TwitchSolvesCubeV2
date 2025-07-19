88c91f52d55a0a8a1894b2060f7d5c6f294f08e5
TwitchSolvesCube <90794191+TwitchSolvesCube@users.noreply.github.com>
2025-07-19 08:59:29 -0500

    Add support for more puzzles

--- client/main.ts
+++ client/main.ts
@@ -1,2 +1,11 @@
 import { twitchClient } from "./twitchClient";
-const client = new twitchClient();
+//Supported
+//const client = new twitchClient("222");
+const client = new twitchClient("333");
+//const client = new twitchClient("444");
+//const client = new twitchClient("555");
+//Not Supported
+//const client = new twitchClient("clock");
+//const client = new twitchClient("pyram");
+//const client = new twitchClient("skewb");
+//const client = new twitchClient("minx");
